afa475f5281ffb74b2834b53ad206d5a426ef642
Luke White <lukewhiteplays@gmail.com>
2014-07-16 20:07:06 -0700

    article and home page

---
+++ content/article.php
@@ -0,0 +1,128 @@
+<?php include "../partials/header.php"; ?>
+<div class="row article-single">
+
+	<div class="col-lg-6 col-lg-offset-2">
+		<div class="page-header">
+			<h1>HCG Weight Loss & Diabetes <small>Find Out How to Lose Up To A Pound A Day.</small></h1>
+			<div class="article-single-author">By <a href="#">Mark Stengler</a></div>
+			<div class="article-single-date">June 1, 2014</div>
+			<div class="article-single-print"><span class="fa fa-2x fa-print"></span></div>
+			<div class="article-single-share">
+				<a href="#">
+					<span class="fa-stack fa-lg">
+						<i class="fa fa-circle fa-stack-2x"></i>
+						<i class="fa fa-facebook fa-stack-1x fa-inverse"></i>
+					</span>
+				</a>
+				<a href="#">
+					<span class="fa-stack fa-lg">
+						<i class="fa fa-circle fa-stack-2x"></i>
+						<i class="fa fa-twitter fa-stack-1x fa-inverse"></i>
+					</span>
+				</a>
+				<a href="#">
+					<span class="fa-stack fa-lg">
+						<i class="fa fa-circle fa-stack-2x"></i>
+						<i class="fa fa-youtube fa-stack-1x fa-inverse"></i>
+					</span>
+				</a>
+				<a href="#">
+					<span class="fa-stack fa-lg">
+						<i class="fa fa-circle fa-stack-2x"></i>
+						<i class="fa fa-envelope fa-stack-1x fa-inverse"></i>
+					</span>
+				</a>
+			</div>
+		</div><!-- ./page-header -->
+		<div class="article-featured-image">
+			<img src="/assets/img/backgrounds/healthy-life-09.jpg" class="img-responsive">
+		</div>
+		
+		<div class="article-content">
+			
+			<span class="dropcap">I</span>was recently diagnosed with Type 2 diabetes and I am concerned about the side effects diabetic drugs can cause. I want to get my health back so I can enjoy time with my children and grandchildren.”
+			
+			<br><br>These were the words of Carolee, a pleasant lady in her early 60s who was referred to my clinic by other family members who are patients of mine. As with <a href="#">many people with diabetes</a>, the real cause of her elevated glucose level was her obesity. She was 100 pounds overweight. In order to avoid diabetic medications, she needed drastic weight loss.
+			
+			<br><br>
+			<span class="article-featured-content">
+				As with many people with diabetes, the real cause of her elevated glucose level was her obesity. She was 100 pounds overweight. In order to avoid diabetic medications, she needed drastic weight loss.
+			</span>
+			While I offer <a href="#">various weight loss programs</a>, I recommended the most aggressive which involves a low calorie diet, supplements, light exercise, and a prescription medication known as human chorionic gonadotrophin (HCG). After several 30 day cycles of HCG (alternated with a higher calorie 30 day maintenance diet and exercise regime), Carolee no longer has diabetes. She has lost 90 pounds and feels a tremendous sense of energy and vitality. Her elevated cholesterol levels have normalized and the arthritis of her knees is gone.
+			
+			<h3>An Overview of Dr. Stengler’s HCG Program</h3>
+			Dr. Simeons certainly made a great contribution to society with his <a href="#">research on HCG combined with a low calorie diet</a>. However, we must realize this diet is over 40 years old and much has changed in our understanding of physiology and nutrition.
+			
+			<br><br>Over the years I have modernized this HCG program in various ways as follows:
+			
+			<ol>
+				<li>The original program involves 3 days of a high protein, high fat-loading phase. I have found this unnecessary for most patients.</li>
+			
+				<li>The original program does not allow for the topical application of cosmetics such as oils and creams. I have not found this to be necessary.</li>
+			
+				<li>The original program forbids the mixing of various vegetables. I have not found this to be necessary and allow the liberal use of vegetables since they are low calorie.</li>
+			
+				<li>The original program does not recommend vitamins and minerals except for calcium, vitamin D, and vitamin C. I have found that supplementation is very helpful in maintaining energy on the program.</li>
+			
+				<li>The original program does not allow any food for breakfast. My program utilizes a low sugar, great tasting whey protein to maintain better energy levels.</li>
+			
+				<li>The original program utilizes HCG in the injection form only. I offer both injection and sublingual HCG (both as an off-label prescription directly from a pharmacy). While most patients choose the sublingual form, my research has shown very similar results with either form. Most patients lose one-half to one pound daily with either technique.
+				</li>
+			</ol>
+			<h3>The History of HCG for Weight Loss</h3>
+			The use of HCG and a low calorie diet for weight loss has an interesting history. It starts with a British born medical doctor by the name of A.T.W. Simeons. Trained at a German medical school, he practiced in India from 1931 until 1949, working on conventional treatments for the testing and treatment of malaria. For his research against malaria, Dr. Simeons was awarded an “Order of Merit” by the Red Cross. In 1949, he transitioned to the Salvador Mundi International Hospital in Rome, where he focused on the treatment of obesity.
+			
+			<br><br>His time in India actually provided the basis for his new weight loss protocol. While in India he noticed that “fat boys” with undescended testicles, when given daily injections of HCG, noticed that not only their testicles descended but that there was a “distinct decrease in the circumference of the hips.” He observed that HCG mobilized fat from the hips and deposited it into more normal fat deposit areas of the body. In addition, he theorized HCG worked to normalize the brain’s set point for metabolism. Dr Simeons’ original protocol is available to <a href="http://www.hcgbodyshaper.com/simeon.pdf">download for free</a>.
+			
+			<br><br>Dr. Simeons’ next step was to use daily HCG injections along with a very low calorie diet (500 calories per day) in overweight patients. He found that patients experienced significant weight loss without hunger problems. (I should note his patients were in a hospital setting. My program involves medical supervision in an outpatient clinic setting with mandatory weekly office visits.)
+			 
+			<br><br>
+			<div class="well"><a href="#">Get started now with Dr. Stengler's HCG Weight Loss Program!</a></div>
+		</div><!-- ./article-content-->
+	</div><!-- ./col-lg-6 -->
+	
+	<div class="col-lg-2 article-single-sidebar">
+		<ul class="nav nav-tabs nav-justified" role="tablist">
+			<li class="active"><a href="#popular">Popular</a></li>
+			<li><a href="#recent">Recent</a></li>
+		</ul>
+		<div class="tab-content">
+			<div class="tab active" role="tab" id="popular">
+				<div class="article-related-article">
+					<a href="#"><img src="/assets/img/placeholder/hypothyroidism_symptoms.png" class="img-responsive" /></a>
+					<a href="#">Symptoms of Hypothyrodism</a>
+				</div>
+				
+				<div class="article-related-article">
+					<a href="#"><img src="/assets/img/backgrounds/ivtherapy_01.jpg" class="img-responsive" /></a>
+					<a href="#">The Promise of IV Therapy Treatment</a>
+				</div>
+				
+				<div class="article-related-article">
+					<a href="#"><img src="/assets/img/backgrounds/background_temp_01.jpg" class="img-responsive" /></a>
+					<a href="#">The Dangers of Fybromyalgia</a>
+				</div>
+			</div>
+		</div>
+		
+		<h3>Related Products</h3>
+			<a href="/products/" class="thumbnail">
+				<img src="http://placehold.it/240x240">
+			</a>
+			<a href="/products/" class="thumbnail">
+				<img src="http://placehold.it/240x240">
+			</a>
+			<a href="/products/" class="thumbnail">
+				<img src="http://placehold.it/240x240">
+			</a>
+			<a href="/products/" class="thumbnail">
+				<img src="http://placehold.it/240x240">
+			</a>
+	</div> <!-- ./article-single-sidebar -->
+	
+</div>
+
+
+
+
+<?php include "../partials/footer.php"; ?>
--- index.php
+++ index.php
@@ -5,8 +5,12 @@
 	</div>
 	<div class="col-lg-6">
 		<div class="row">
-			<div class="col-lg-6">
-				<img src="http://placehold.it/450x535" class="img-responsive">
+			<div class="col-lg-6 feature" style="background-image:url(/assets/img/placeholder/cholesterol-under-control.jpg);">
+				<div class="caption fade in">
+					<h3>More than Cholesterol<small>by Mark Stengler</small></h3>
+					<p class="lead">Research has demonstrated that the lipid doesn't assess your cardiovascular risk.</p>
+					<a href="/content/article.php">Read More <span class="fa fa-arrow-right"></span></a>
+				</div>
 			</div>
 			<div class="col-lg-6">
 				<img src="http://placehold.it/450x535" class="img-responsive">
