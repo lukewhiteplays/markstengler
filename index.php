4c2e8c37d2c88c001157fb4308efd29f4bfb23f8
Luke White <lukewhiteplays@gmail.com>
2014-07-26 18:33:45 -0700

    home page adj

--- index.php
+++ index.php
@@ -1,4 +1,44 @@
 <?php include "partials/header.php"; ?>
+<div id="carousel-example-generic" class="features carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner">
+    <div class="item active">
+      <img src="http://placehold.it/1900x800" alt="...">
+      <div class="carousel-caption">
+        <h1>This is a caption</h1>
+      </div>
+    </div>
+    <div class="item">
+      <img src="http://placehold.it/1900x800" alt="...">
+      <div class="carousel-caption">
+        <h1>This is another caption</h1>
+      </div>
+    </div>
+    <div class="item">
+      <img src="http://placehold.it/1900x800" alt="...">
+      <div class="carousel-caption">
+        <h1>This is yet another caption</h1>
+      </div>
+    </div>
+  </div>
+
+  <!-- Controls -->
+  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left"></span>
+  </a>
+  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right"></span>
+  </a>
+</div>
+
+
 <div class="row features">
 	<div class="col-lg-6 feature-video-large">
 		<img src="/assets/img/placeholder/videoPlaceholder.jpg" width="720" class="img-responsive">
@@ -12,8 +52,12 @@
 					<a href="/content/article.php">Read More <span class="fa fa-arrow-right"></span></a>
 				</div>
 			</div>
-			<div class="col-lg-6">
-				<img src="http://placehold.it/450x535" class="img-responsive">
+			<div class="col-lg-6 feature feature-alt" style="background-image:url(/assets/img/backgrounds/healthy-life-09-400x266.jpg);">
+				<div class="caption fade in">
+					<h3>HCG Weight Loss<small>by Mark Stengler</small></h3>
+					<p class="lead">Over the years I have modernized this HCG program in various ways</p>
+					<a href="/content/article.php">Read More <span class="fa fa-arrow-right"></span></a>
+				</div>
 			</div>
 		</div>
 	</div>	
